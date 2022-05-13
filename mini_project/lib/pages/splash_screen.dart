e5c02b44eebdf28e085f10f480044197c81af569
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-05-13 21:51:15 +0700

    Tugas Mini Project

---
+++ mini_project/lib/pages/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'auth_page.dart';
+import 'package:auth_project/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // set time to load the new page
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: Lottie.asset("assets/splashtitle.json"),
+            ),
+            Text(
+              "Eventder",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

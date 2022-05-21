8861c1668b201a8f19764859de6dda6cd609ad7c
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-05-21 18:10:31 +0700

    Tugas Mini Project

--- mini_project_fix/eventder/lib/view_models/event_view_model.dart
+++ mini_project_fix/eventder/lib/view_models/event_view_model.dart
@@ -3,17 +3,17 @@ import 'package:eventder/models/event.dart';
 
 class EventViewModel extends ChangeNotifier {
   bool _loading = false;
-  List<Event> _eventListModel = [];
+  List<Event> _eventViewModel = [];
 
   bool get loading => _loading;
-  List<Event> get eventListModel => _eventListModel;
+  List<Event> get eventViewModel => _eventViewModel;
 
   setLoading(bool loading) async {
     _loading = loading;
     notifyListeners();
   }
 
-  setEventListModel(List<Event> eventListModel) {
-    _eventListModel = eventListModel;
+  setEventViewModel(List<Event> eventViewModel) {
+    _eventViewModel = eventViewModel;
   }
 }

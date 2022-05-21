f10f3d5ad14dec277218471103a05656a3ecdd40
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-05-21 17:45:28 +0700

    Tugas Mini Project

---
+++ mini_project_fix/eventder/lib/view_models/event_list_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:eventder/controller/sqlite.dart';
+import 'package:eventder/models/event.dart';
+
+class EventViewModel extends ChangeNotifier {
+  bool _loading = false;
+  List<Event> _eventListModel = [];
+
+  bool get loading => _loading;
+  List<Event> get eventListModel => _eventListModel;
+
+  setLoading(bool loading) async {
+    _loading = loading;
+    notifyListeners();
+  }
+
+  setEventListModel(List<Event> eventListModel) {
+    _eventListModel = eventListModel;
+  }
+}

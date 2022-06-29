b0ef518b158de3e59b27c3ff03c252ccd30e5e8a
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-06-29 15:02:20 +0700

    Mini Project Eventder

--- mini_project_fix/eventder/lib/widgets/event_item.dart
+++ mini_project_fix/eventder/lib/widgets/event_item.dart
@@ -41,94 +41,96 @@ class _EvenItemViewState extends State<EvenItemView> {
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(2.0),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: Container(
-                    decoration: const ShapeDecoration(
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10)))),
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.network(
-                      widget.event.performers![0].image,
-                      fit: BoxFit.cover,
-                      height: 80,
-                      width: 120,
-                    )),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.event.title!,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4),
-                        child: Text(
-                          widget.event.venue!.displayLocation!,
+          child: SingleChildScrollView(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Container(
+                      decoration: const ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)))),
+                      clipBehavior: Clip.antiAlias,
+                      child: Image.network(
+                        widget.event.performers![0].image,
+                        fit: BoxFit.cover,
+                        height: 80,
+                        width: 120,
+                      )),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.event.title!,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4),
+                          child: Text(
+                            widget.event.venue!.displayLocation!,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black54),
+                          ),
+                        ),
+                        Text(
+                          parseDate(widget.event.datetimeUtc!),
                           style: const TextStyle(
                               fontWeight: FontWeight.normal,
                               color: Colors.black54),
-                        ),
-                      ),
-                      Text(
-                        parseDate(widget.event.datetimeUtc!),
-                        style: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black54),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              !kIsWeb
-                  ? Center(
-                      child: CupertinoButton(
-                        child: widget.event.favourite
-                            ? const Icon(
-                                MaterialCommunityIcons.heart,
-                                color: CupertinoColors.systemRed,
-                              )
-                            : const Icon(
-                                MaterialCommunityIcons.heart_outline,
-                                color: CupertinoColors.systemRed,
-                              ),
-                        onPressed: () async {
-                          Favourite _favourite = Favourite(
-                              photo: widget.event.performers![0].image,
-                              location: widget.event.venue!.displayLocation!,
-                              eventTime: widget.event.datetimeUtc!,
-                              eventId: widget.event.id!.toString(),
-                              title: widget.event.title);
-                          if (!widget.event.favourite) {
-                            var stat =
-                                await Bhandaram.db!.addFavourite(_favourite);
-                            if (stat > 0) {
-                              widget.event.favourite = true;
-                              widget.onFavUpdate(true);
+                !kIsWeb
+                    ? Center(
+                        child: CupertinoButton(
+                          child: widget.event.favourite
+                              ? const Icon(
+                                  MaterialCommunityIcons.heart,
+                                  color: CupertinoColors.systemRed,
+                                )
+                              : const Icon(
+                                  MaterialCommunityIcons.heart_outline,
+                                  color: CupertinoColors.systemRed,
+                                ),
+                          onPressed: () async {
+                            Favourite _favourite = Favourite(
+                                photo: widget.event.performers![0].image,
+                                location: widget.event.venue!.displayLocation!,
+                                eventTime: widget.event.datetimeUtc!,
+                                eventId: widget.event.id!.toString(),
+                                title: widget.event.title);
+                            if (!widget.event.favourite) {
+                              var stat =
+                                  await Bhandaram.db!.addFavourite(_favourite);
+                              if (stat > 0) {
+                                widget.event.favourite = true;
+                                widget.onFavUpdate(true);
+                              }
+                            } else {
+                              var status =
+                                  await Bhandaram.db!.deleteExpense(_favourite);
+                              if (status > 0) {
+                                widget.event.favourite = false;
+                                widget.onFavUpdate(false);
+                              }
                             }
-                          } else {
-                            var status =
-                                await Bhandaram.db!.deleteExpense(_favourite);
-                            if (status > 0) {
-                              widget.event.favourite = false;
-                              widget.onFavUpdate(false);
-                            }
-                          }
-                        },
-                      ),
-                    )
-                  : Container()
-            ],
+                          },
+                        ),
+                      )
+                    : Container()
+              ],
+            ),
           ),
         ),
       ),
